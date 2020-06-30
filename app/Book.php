bacffa88278c26836983d4227411086e8acd8f9d
Illia Antonenko <hespaff@gmail.com>
2020-06-30 14:57:59 +0300

    adds new path to book resource

--- app/Book.php
+++ app/Book.php
@@ -2,9 +2,26 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Book
+ * @package App
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $author
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ *
+ */
 class Book extends Model
 {
     protected $guarded = [];
+
+    public function path()
+    {
+        return route('books.show', $this->id);
+    }
 }
